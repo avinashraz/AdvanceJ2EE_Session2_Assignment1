e616cb683447485a6d5f0ce3ada2cdc368c50066
avinashraz <avinashaninthaneni@gmail.com>
2016-10-19 12:31:30 +0530

    Eligible to vote

---
+++ src/EligibleToVote.java
@@ -0,0 +1,16 @@
+
+public class EligibleToVote {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int age=18;
+System.out.println("Enter Your Age:"+age);
+if(age >=18){
+	//age greater than equal to 18
+	System.out.println("Your Eligible To Vote");
+}else{
+	System.out.println("Your Not Eligible");
+}
+	}
+
+}
